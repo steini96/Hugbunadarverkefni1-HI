f38b60f01e29682a15039447cb1a3efac124022e
steini96 <steini.tki@gmail.com>
2021-10-06 09:35:39 +0000

    Set up the spring project

---
+++ Hugbunadarverkefni1/src/main/java/com/example/hugbunadarverkefni1/Hugbunadarverkefni1Application.java
@@ -0,0 +1,13 @@
+package com.example.hugbunadarverkefni1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hugbunadarverkefni1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hugbunadarverkefni1Application.class, args);
+    }
+
+}
---
+++ Hugbunadarverkefni1/src/test/java/com/example/hugbunadarverkefni1/Hugbunadarverkefni1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hugbunadarverkefni1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hugbunadarverkefni1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
