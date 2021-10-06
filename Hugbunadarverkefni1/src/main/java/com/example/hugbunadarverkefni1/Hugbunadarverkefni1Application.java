498a74348b937ba1658c58c26fc03a31a95f9a22
steini96 <steini.tki@gmail.com>
2021-10-06 09:41:50 +0000

    Add a hello world site

--- Hugbunadarverkefni1/src/main/java/com/example/hugbunadarverkefni1/Hugbunadarverkefni1Application.java
+++ Hugbunadarverkefni1/src/main/java/com/example/hugbunadarverkefni1/Hugbunadarverkefni1Application.java
@@ -2,12 +2,20 @@ package com.example.hugbunadarverkefni1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Hugbunadarverkefni1Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Hugbunadarverkefni1Application.class, args);
     }
 
+    @GetMapping("/hello")
+    public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
+    }
 }
